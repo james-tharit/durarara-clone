be7970d9db1bb5502e2b2df5ee30598e049ec323
mr.frog <tharit.rt@gmail.com>
2025-07-15 23:50:32 +0700

    feat: added chat page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,12 @@
+import './Home.css'
 export const Home = () => {
   return (
-    <>
-      <p>THIS iS APP</p>
-    </>
+    <div>
+      <div className="chat-container" >
+        <p style={{ color: 'black', fontWeight: 'bold', padding: 0, margin: 0 }}>dollaschat</p>
+        <textarea />
+        <button> post! </button>
+      </div>
+    </div>
   );
 }
