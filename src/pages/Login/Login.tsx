e09e3d38c2271f1705daac8b0206f70a87bf388d
mr.frog <tharit.rt@gmail.com>
2025-07-15 15:43:44 +0700

    feat: implemented form and handle error

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,13 +1,24 @@
-import { useEffect } from 'react'
+import { useRef, useState } from 'react'
 import './Login.css'
 import { Dollas } from '../../components/Dollas.tsx'
 import { authenticate } from '../../authentication/authen'
 
 export const Login = () => {
+  // This ensures the *entire* string consists only of alphanumeric characters.
+  const alphanumericRegex = /^[a-zA-Z0-9]+$/;
 
-  useEffect(() => {
-    authenticate();
-  }, [])
+  const keywordRef = useRef<HTMLInputElement>(null);
+  const [error, setError] = useState('');
+
+  function submit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+    const keyword = keywordRef.current?.value ?? '';
+
+    if (keyword.trim() == '') { setError('can not be empty'); return; }
+    if (!alphanumericRegex.test(keyword.trim())) { setError('can not have special characters'); return; }
+    setError('');
+    authenticate(keyword);
+  }
 
   return (
     <div className='container'>
@@ -15,16 +26,18 @@ export const Login = () => {
         <Dollas />
       </div>
       <div className='password-container'>
-        <div className='input-container' >
-          <p style={{ paddingRight: "8px" }}>Password </p>
-          <input type='password' ></input>
-        </div>
-        <div>
-          <button onClick={() => console.warn("no function")}>
-            <p style={{ padding: '0px 8vh', margin: '0' }}> ENTER</p>
-          </button>
-        </div>
-
+        <form onSubmit={submit}>
+          <div className='input-container' >
+            <p style={{ paddingRight: "8px" }}>Password </p>
+            <input ref={keywordRef} name='keyword' type='password' ></input>
+          </div>
+          <div className='button-container'>
+            <button type='submit' onClick={() => console.warn("no function")}>
+              <p style={{ padding: '0px 8vh', margin: '0' }}> ENTER</p>
+            </button>
+            {error ? <p style={{ fontSize: 'xx-small', color: 'red', fontWeight: 'bold' }}>{error}</p> : <></>}
+          </div>
+        </form>
       </div>
     </div>
   )
