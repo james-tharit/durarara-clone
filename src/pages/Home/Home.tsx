e17b0c326e72c4f72635076bb1ea1bd92d92de07
mr.frog <tharit.rt@gmail.com>
2025-07-20 20:08:39 +0700

    feat: implemented go to chat functionality

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,18 +3,25 @@ import { checkToken } from '../../authentication/authen';
 import './Home.css'
 export const Home = () => {
   const navigator = useNavigate()
+
   function logout() {
     localStorage.removeItem('token')
     if (!checkToken()) navigator('/login')
   }
 
+  function toChat() {
+    navigator('/chat')
+  }
+
   return (
     <div>
-      <div className="home-body" >
-        <div>
+      <div className="home-container" >
+        <div className="home-header">
           <h1 style={{ color: 'black', fontWeight: 'bold', padding: 0, margin: 0 }}>Welcome to Dollas</h1>
           <button onClick={logout} > logout</button>
-
+        </div>
+        <div className="home-body">
+          <button onClick={toChat}>go to chat</button>
         </div>
       </div>
     </div >
